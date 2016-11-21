2e46794a9bc1e2c00066c6666daf8564d27153b4
Mitch Conquer <mitch.conquer@gonimbly.com>
2016-11-20 16:44:11 -0800

    Move filters into own branch of state, move media to own branch called subtitles

---
+++ app/reducers/subtitles.js
@@ -0,0 +1,34 @@
+// @flow
+import { REMOVE_MEDIA, RESET_MEDIA, UPDATE_MEDIA } from '../actions/media';
+
+export const initialState = {};
+
+export default function subtitles(state = initialState, action = {}) {
+  let newState = {};
+  switch (action.type) {
+    case REMOVE_MEDIA:
+      newState = {}
+      Object.keys(state).forEach(index => {
+        if (parseInt(index) !== action.index) {
+          newState[index] = {...state[index]}
+        }
+      })
+      return newState
+    case RESET_MEDIA:
+      action.media.forEach(item => {
+        newState[item.index] = item;
+      });
+      return newState;
+    case UPDATE_MEDIA:
+      newState = {};
+      Object.keys(state).forEach(key => {
+        if (state[key].index !== action.updatedMedia.index) {
+          newState[key] = {...state[key]};
+        }
+      });
+      newState[action.updatedMedia.index] = action.updatedMedia;
+      return newState;
+    default:
+      return state;
+  }
+}
