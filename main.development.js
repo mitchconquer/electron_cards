8a2013fed434fa6d5adbf81e5b3ad4e015d77ef4
Mitch Conquer <mitch.conquer@gonimbly.com>
2017-03-29 20:40:02 -0700

    Set min width on window

--- main.development.js
+++ main.development.js
@@ -43,7 +43,8 @@ app.on('ready', async () => {
   mainWindow = new BrowserWindow({
     show: false,
     width: 1324,
-    height: 728
+    height: 728,
+    minWidth: 1000
   });
 
   mainWindow.loadURL(`file://${__dirname}/app/app.html`);
